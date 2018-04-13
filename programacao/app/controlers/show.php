fe0e1ce9823100cced436b6ddce4287267297ca4
maluu <souzamalu2016@gmail.com>
2018-04-13 13:54:55 -0300

    Add files via upload

---
+++ programacao/app/controlers/show.php
@@ -0,0 +1,6 @@
+<div class"submenu">
+<a href="">editar categoria</a>
+<a href="">excluir categoria</a>
+</div>
+<h2>Detalhes de  Categoria - <?= $categoria->getNome()?></h2>
+<p><b>Descrição:</b><?=$categoria->getDescricao()?> </p>
